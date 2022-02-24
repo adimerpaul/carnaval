a73f2f18fbdcb499958244d58c33babefc0f42aa
adimerpaul <adimer101@gmail.com>
2022-02-24 09:23:23 -0400

    Update 2014_10_12_000000_create_users_table.php

--- carback/database/migrations/2014_10_12_000000_create_users_table.php
+++ carback/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
+        Schema::defaultStringLength(255);
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
