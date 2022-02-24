e08fc3a2a95c877214dfdbaac9b3d3deae170c24
adimerpaul <adimer101@gmail.com>
2022-02-24 09:22:01 -0400

    Update RubroSeeder.php

--- carback/database/seeders/RubroSeeder.php
+++ carback/database/seeders/RubroSeeder.php
@@ -16,7 +16,7 @@ class RubroSeeder extends Seeder
     public function run()
     {
         DB::table("rubros")->insert([
-            ["nombre"=>"Inicio","descripcion"=>"Pagina de inicio","icono"=>"o_home","url"=>"/"],
+            ["nombre"=>"Ruta","descripcion"=>"En tiempo real","icono"=>"o_map","url"=>"/"],
             ["nombre"=>"Supermercados","descripcion"=>"Compra en tiendas","icono"=>"o_shopping_cart","url"=>"/rubro/1"],
             ["nombre"=>"Gastronomia","descripcion"=>"","icono"=>"home","url"=>"/rubro/2"],
         ]);
